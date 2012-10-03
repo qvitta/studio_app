9f4f3b473420c914c471382768a745955760e6f5
Hector Luis <ticaje@free-social.net>
2012-10-04 00:39:37 +0200

    Refactor application_helper
      * Extracted body_classer to it's own file
      * Extracted show_attributes_for helper to qvitta_admin gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,37 +1,7 @@
-class BodyClasser
-  attr_reader :helper, :collection
-
-  delegate :controller_name, :action_name, to: :helper
-
-  # BodyClasser.new(helper)
-  def initialize(helper)
-    @helper = helper
-  end
-
-  def classes
-    [controller_name, action_name, "#{controller_name}-#{action_name}"]
-  end
-
-  def to_html
-    classes.join(' ')
-  end
-end
+require 'body_classer'
 
 module ApplicationHelper
   def body_classes
     BodyClasser.new(self).to_html
-  end
-
-  def show_attributes_for(model)
-    case model.name
-    when "Project"
-      [ :name, :description, :updated_at ]
-    when "Event"
-      [ :name, :location, :due_at ]
-    when "Task"
-      [ :name, :done ]
-    else
-      [ :id, :updated_at ]
-    end
   end
 end
---
+++ lib/body_classer.rb
@@ -0,0 +1,18 @@
+class BodyClasser
+  attr_reader :helper, :collection
+
+  delegate :controller_name, :action_name, to: :helper
+
+  # BodyClasser.new(helper)
+  def initialize(helper)
+    @helper = helper
+  end
+
+  def classes
+    [controller_name, action_name, "#{controller_name}-#{action_name}"]
+  end
+
+  def to_html
+    classes.join(' ')
+  end
+end
