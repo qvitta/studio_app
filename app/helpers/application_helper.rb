980968c8f33349af0e6015440df72ad86b872e0c
Hector Luis <ticaje@free-social.net>
2012-10-03 22:52:01 +0200

    Move helpers to qvitta_admin gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,29 +19,7 @@ end
 
 module ApplicationHelper
   def body_classes
-    #binding.pry
     BodyClasser.new(self).to_html
-  end
-
-  def admin_menu_link(resources)
-    text = I18n.t("admin.menu.#{resources}", default: resources.to_s.titleize)
-    menu_link(text, [:admin, resources], resources)
-  end
-
-  def menu_link(text, url, current)
-    active = controller_name == current.to_s ? "active" : nil
-    content_tag(:li, class: active) do
-      link_to text, url
-    end
-  end
-
-  def my_simple_form(resource, &block)
-    form_url = resource.new_record? ? collection_url : resource_url(resource)
-    simple_form_for(resource, { url: form_url,  html: { class: 'form-vertical'} }, &block)
-  end
-
-  def form_attributes_for(model)
-    model.accessible_attributes.select {|attr| !attr.blank? }
   end
 
   def show_attributes_for(model)
